39d30a1080af593af601bf16612a13ef53233256
Tanvir3ds <tanayeem15@gmail.com>
2020-03-07 20:09:02 +0600

    first commit

---
+++ app_add_to_cart/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppAddToCartConfig(AppConfig):
+    name = 'app_add_to_cart'
---
+++ app_add_to_cart/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+# Create your views here.
+def home(request):
+    return render(request, 'base.html')
+    
